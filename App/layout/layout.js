4cbef93e0c24626312ea93683c87ecce68926602
Vedran Bilopavlović <vbilopav@gmail.com>
2014-09-03 17:06:16 +0200

    new search bar

--- App/layout/layout.js
+++ App/layout/layout.js
@@ -1,20 +1,21 @@
 ﻿define([
-    'text!layout/layout.html',
-    'layout/nocollapse'
-], function (template, nocollapse) {
+    'text!layout/layout.html'
+], function (template) {
 
     var nav = null,
+        navmain = null,
         navbrand = null,
         navtoggle = null,
         navcollapse = null,
-        navsmall = null,
-        navnocollapse = null,
         toggleVisible = null;
 
-
     var onClickNavigation = function (e) {
-        if (e.target.href === window.location.href && navtoggle.is(':visible') && navcollapse.is(':visible')) {
+        if (e.target.href === window.location.href && navtoggle.is(':visible')) {
             navtoggle.trigger("click");
+        } else {
+            if (e.target.href === undefined && navtoggle.is(':visible') && navcollapse.is(':visible')) {
+                navtoggle.trigger("click");
+            }
         }
     };
 
@@ -26,13 +27,11 @@
 
     var onNavToggleShow = function (shown) {
         if (shown) {
-            $('input', navsmall).val($('input', navnocollapse).val());
-            navsmall.css('display', 'block');
-            navnocollapse.css('display', 'none');
+            nav.show();
+            navmain.hide();
         } else {
-            $('input', navnocollapse).val($('input', navsmall).val());
-            navsmall.css('display', 'none');
-            navnocollapse.css('display', 'block');
+            nav.hide();
+            navmain.show();
         }
     };
 
@@ -44,36 +43,33 @@
             if (!navtoggle.is(':visible') && toggleVisible) {
                 toggleVisible = false;
                 onNavToggleShow(toggleVisible);
+                //navcollapse.hide();
             }
     };
 
     // these links populate the navbar
     var viewModel = kendo.observable({
         home: 'Kendo UI By Telerik',
+        signIn: 'SignIn!',
         navclick: onClickNavigation,
         links: [{ title: 'Home', href: '#/', icon: 'home', icon: 'fa fa-home' },
                 { title: 'Details', href: '#/details', icon: 'fa fa-ellipsis-h' },
                 { title: 'Missing', href: '#/missing', icon: 'fa fa-bug' }]
     });
 
-    $.subscribe('/router/change', function (e) {
-
-        //if toggle nav menu is collapsed, uncollapse it whwn view is shown
-        collapseNavMenu();
-
-        nav.find('span').removeClass('lb-color');
-        if (e.url === '/') {
-            navbrand.children(0).addClass("lb-color");
-            nav.find('li').removeClass('active');
-        } else {
-            navbrand.children(0).removeClass("lb-color");
-            var active = nav.find('a[href="#' + e.url + '"]').parent();
-            nav.find('li').removeClass('active');
-            if (active.length > 0) {
-                active.addClass('active');
-                active.children(0).children(0).addClass("lb-color");
-            }
+    var activeteSelectedNav = function (el, url) {
+        var active = el.find('a[href="#' + url + '"]').parent();
+        el.find('li').removeClass('active');
+        if (active.length > 0) {
+            active.addClass('active');
+            active.children(0).children(0).addClass("lb-color");
         }
+    }
+
+    $.subscribe('/router/change', function (e) {
+        collapseNavMenu();
+        activeteSelectedNav(nav, e.url);
+        activeteSelectedNav(navmain, e.url);
     });
 
     $(window).resize(onWinResize);
@@ -86,14 +82,15 @@
         init: function (e) {
 
             //cache elements and do initialization
-            nav = e.sender.element.find('#nav-links');
-            navbrand = e.sender.element.find('.navbar-brand');
-            navtoggle = $('.navbar-header .navbar-toggle');
+            var h = $('#navbar-header');
+            nav = e.sender.element.find('#nav-links', h);
+            navmain = e.sender.element.find('#nav-links-main', h);
+            navbrand = e.sender.element.find('.navbar-brand', h);
+            navtoggle = $('#navbar-toggle', h);
             navcollapse = $('#navbar-collapse');
+
             toggleVisible = navtoggle.is(':visible');
-            navsmall = $('ul#nav-small').html(nocollapse.html);
-            navnocollapse = $('ul#nav-nocollapse').html(nocollapse.html);
-            onNavToggleShow(toggleVisible);
+            onNavToggleShow(toggleVisible);        
         }
     });
 
